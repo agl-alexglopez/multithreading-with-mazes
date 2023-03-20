c5a2a22db51803334909a577d37dd8fdf04a4468
Alex Lopez <agl.alexglopez@gmail.com>
2023-03-20 09:49:12 -0700

    add usage instructions

--- run_maze.cc
+++ run_maze.cc
@@ -12,6 +12,7 @@ const std::unordered_set<std::string> argument_flags = {
 void set_relevant_arg(Thread_maze::Packaged_args& maze_args,
                       std::string_view flag,
                       std::string_view arg);
+void print_usage();
 
 int main(int argc, char **argv) {
     Thread_maze::Packaged_args maze_args = {};
@@ -30,6 +31,7 @@ int main(int argc, char **argv) {
                 const auto& found_arg = argument_flags.find(arg.data());
                 if (found_arg == argument_flags.end()) {
                     std::cerr << "Invalid argument parameter: " << arg << std::endl;
+                    print_usage();
                     std::abort();
                 }
                 process_current = true;
@@ -56,6 +58,7 @@ void set_relevant_arg(Thread_maze::Packaged_args& maze_args,
             maze_args.builder = Thread_maze::Builder_algorithm::randomized_loop_erased;
         } else {
             std::cerr << "Invalid builder argument: " << arg << std::endl;
+            print_usage();
             std::abort();
         }
     } else if (flag == "-s") {
@@ -65,7 +68,37 @@ void set_relevant_arg(Thread_maze::Packaged_args& maze_args,
             maze_args.solver = Thread_maze::Solver_algorithm::breadth_first_search;
         } else {
             std::cerr << "Invalid solver argument: " << arg << std::endl;
+            print_usage();
             std::abort();
         }
     }
+}
+
+void print_usage() {
+    std::cout << "┌──────────────────────────────────────────────────────┐\n"
+              << "│                                                      │\n"
+              << "├────────────Thread Maze Usage Instructions────────────┤\n"
+              << "│                                                      │\n"
+              << "│  Use flags, followed by arguments, in any order:     │\n"
+              << "│                                                      │\n"
+              << "│  -r - Rows flag. Set rows for the maze.              │\n"
+              << "│      - Any number > 5. Zoom out for larger mazes!    │\n"
+              << "│  -c - Columns flag. Set columns for the maze.        │\n"
+              << "│      - Any number > 7. Zoom out for larger mazes!    │\n"
+              << "│  -b - Builder flag. Set maze building algorithm.     │\n"
+              << "│      - random-dfs - Randomized Depth First Search    │\n"
+              << "│      - loop-erase - Loop-Erased Random Walk          │\n"
+              << "│  -s - Solver flag. Set maze solving algorithm.       │\n"
+              << "│      - dfs - Depth First Search                      │\n"
+              << "│      - bfs - Breadth First Search                    │\n"
+              << "│                                                      │\n"
+              << "│  If any flags are omitted, defaults are used.        │\n"
+              << "│                                                      │\n"
+              << "│  Examples:                                           │\n"
+              << "│  ./run_maze                                          │\n"
+              << "│  ./run_maze -r 51 -c 111 -b random-dfs -s bfs        │\n"
+              << "│  ./run_maze -r 51 -c 111 -s bfs                      │\n"
+              << "│                                                      │\n"
+              << "└──────────────────────────────────────────────────────┘\n";
+    std::cout << std::endl;
 }
