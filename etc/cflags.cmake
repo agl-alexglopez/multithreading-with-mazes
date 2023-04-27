a807e78c3a85424297b8a1d897ba4bf4ebf4074d
Alex Lopez <agl.alexglopez@gmail.com>
2023-04-27 12:03:37 -0700

    add compiler specific flags

--- etc/cflags.cmake
+++ etc/cflags.cmake
@@ -5,4 +5,13 @@ set(SANITIZING_FLAGS -fno-sanitize-recover=all -fsanitize=undefined -fsanitize=a
 
 # ask for more warnings from the compiler
 set (CMAKE_BASE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
-set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wpedantic -Wextra -Weffc++ -Werror -Wshadow -Wpointer-arith -Wcast-qual -Wformat=2 -Wno-unqualified-std-cast-call")
+
+# AppleClang specific flags
+if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "AppleClang")
+    set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wpedantic -Wextra -Weffc++ -Werror -Wshadow -Wpointer-arith -Wcast-qual -Wformat=2")
+endif()
+
+# GCC or Clang specific flags
+if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang" OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+    set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wpedantic -Wextra -Weffc++ -Werror -Wshadow -Wpointer-arith -Wcast-qual -Wformat=2 -Wno-unqualified-std-cast-call")
+endif()
