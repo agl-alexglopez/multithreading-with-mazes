75f9c87ba1ce050e63a77079029e1a072449b88c
Alex Lopez <agl.alexglopez@gmail.com>
2023-11-16 01:38:39 -0800

    cmake change

--- etc/cflags.cmake
+++ etc/cflags.cmake
@@ -2,12 +2,6 @@ set (CMAKE_CXX_STANDARD 20)
 set (CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
 
-# Maybe this works, idk. Not getting any warnings.
-if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-    add_compile_options(-fno-sanitize-recover=all -fsanitize=undefined -fsanitize=address)
-    add_link_options(-fno-sanitize-recover=all -fsanitize=undefined -fsanitize=address)
-endif()
-
 # ask for more warnings from the compiler
 set(CMAKE_BASE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
 
@@ -18,5 +12,11 @@ endif()
 
 # GCC or Clang specific flags
 if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang" OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-    set (CMAKE_CXX_FLAGS "${CMAKE_BASE_CXX_FLAGS} -g3 -Wall -Wpedantic -Wextra -Weffc++ -Werror -Wshadow -Wpointer-arith -Wcast-qual -Wformat=2 -Wno-unqualified-std-cast-call")
+    # Maybe this works, idk. Not getting any warnings.
+    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+        set (CMAKE_CXX_FLAGS "${CMAKE_BASE_CXX_FLAGS} -g3 -Wall -Wpedantic -Wextra -Weffc++ -Werror -Wshadow -Wpointer-arith -Wcast-qual -Wformat=2 -Wno-unqualified-std-cast-call -fno-sanitize-recover=all -fsanitize=undefined -fsanitize=address")
+        add_link_options(-fno-sanitize-recover=all -fsanitize=undefined -fsanitize=address)
+    else()
+        set (CMAKE_CXX_FLAGS "${CMAKE_BASE_CXX_FLAGS} -Wall -Wpedantic -Wextra -Weffc++ -Werror -Wshadow -Wpointer-arith -Wcast-qual -Wformat=2 -Wno-unqualified-std-cast-call")
+    endif()
 endif()
